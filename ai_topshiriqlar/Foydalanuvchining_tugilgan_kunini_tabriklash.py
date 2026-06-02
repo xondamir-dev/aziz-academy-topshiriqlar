4c6739c06ef3d3256ceec51d68feda2c7d13920e
xondamir917-bit <xondamir917@gmail.com>
2026-06-02 09:00:47 +0500

    Add AI task: Foydalanuvchining tug'ilgan kunini tabriklash

---
+++ ai_topshiriqlar/Foydalanuvchining_tugilgan_kunini_tabriklash.py
@@ -0,0 +1,18 @@
+# Foydalanuvchining tug'ilgan kunini tabriklash
+# Kurs: IT Dasturlash
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+malumot = input().split()
+isim = malumot[0]
+tug_yil = int(malumot[1])
+tug_oy = int(malumot[2]) 
+tug_kun = int(malumot[3])
+
+b = 15
+
+if tug_kun >= b:
+    print(tug_kun - b)
+else:
+    print(31 - b + 9)
