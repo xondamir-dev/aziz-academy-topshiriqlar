9badb5aba92c2094d3c248e60c46241fac6f5bc1
xondamir917-bit <xondamir917@gmail.com>
2026-06-02 08:48:01 +0500

    Add AI task: Do'kon uchun mahsulotni saralash dasturi

---
+++ ai_topshiriqlar/Dokon_uchun_mahsulotni_saralash_dasturi.py
@@ -0,0 +1,12 @@
+# Do'kon uchun mahsulotni saralash dasturi
+# Kurs: IT Dasturlash
+# Mavzu: input() bilan foydalanuvchidan ma’lumot olish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+mahsulot_nomi = input()
+narx = int(input())
+saralash_harfi = input()
+
+if mahsulot_nomi [0] == saralash_harfi:
+    print(f"{mahsulot_nomi} {narx}")
