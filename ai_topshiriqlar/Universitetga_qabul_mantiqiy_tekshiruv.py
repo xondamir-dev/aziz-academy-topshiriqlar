dca9a207be82e80c42d2d10fe4296d7b1d50915f
xondamir917-bit <xondamir917@gmail.com>
2026-06-04 09:27:06 +0500

    Add AI task: Universitetga qabul: mantiqiy tekshiruv

---
+++ ai_topshiriqlar/Universitetga_qabul_mantiqiy_tekshiruv.py
@@ -0,0 +1,12 @@
+# Universitetga qabul: mantiqiy tekshiruv
+# Kurs: IT Dasturlash
+# Mavzu: Mantiqiy operatorlar: and, or, not
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+i, b = map(int, input().split())
+
+if i > 50 and b > 60:
+    print("Qabul qilindi")
+else:
+    print("Qabul qilinmadi")
