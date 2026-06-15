d9b434d21ce93f96f6995f8c545842a8d25c5952
xondamir-dev <xondamir917@gmail.com>
2026-06-15 13:45:34 +0500

    Add AI task: Matnni qayta ishlash

---
+++ ai_topshiriqlar/Matnni_qayta_ishlash.py
@@ -0,0 +1,15 @@
+# Matnni qayta ishlash
+# Kurs: Dasturlash / IT
+# Mavzu: Ma’lumot turlari: str (string) kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+input_str = input().split()
+ism = input_str[0]
+familiya = input_str[1]
+tugilgan_yili = int(input_str[2])
+
+joriy_yil = 2023 
+yosh = joriy_yil - tugilgan_yili
+print(ism + " " + familiya, yosh)
