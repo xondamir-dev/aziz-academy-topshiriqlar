4212c30a793ced460f71a9ce93ef1df10a5c29b7
xondamir-dev <xondamir917@gmail.com>
2026-06-23 15:50:01 +0500

    Add AI task: Do'kondagi mahsulotlar sonini hisoblash

---
+++ ai_topshiriqlar/Dokondagi_mahsulotlar_sonini_hisoblash.py
@@ -0,0 +1,16 @@
+# Do'kondagi mahsulotlar sonini hisoblash
+# Kurs: Dasturlash / IT
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+j = 0
+while True:
+    try:
+        mahsulot = input()
+        n, s = mahsulot.split()
+        j +=  int(s)
+    except:
+        break
+        
+print(f"Jami mahsulotlar soni: {j}")
