916751b765719024a11245a528e68aa13a7b21fb
xondamir-dev <xondamir917@gmail.com>
2026-06-24 13:43:57 +0500

    Add AI task: Kinoteatrda chipta sotish dasturi

---
+++ ai_topshiriqlar/Kinoteatrda_chipta_sotish_dasturi.py
@@ -0,0 +1,14 @@
+# Kinoteatrda chipta sotish dasturi
+# Kurs: Dasturlash / IT
+# Mavzu: if, elif, else asoslari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+yosh = int(input())
+
+if yosh < 6:
+    print(0)
+elif yosh < 18 and yosh >= 7:
+    print(15000)
+else:
+    print(20000)
