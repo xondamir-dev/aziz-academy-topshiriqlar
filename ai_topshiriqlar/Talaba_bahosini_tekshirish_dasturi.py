dbcd98858b67a913112d1e52c812c0d02356a5a2
xondamir-dev <xondamir917@gmail.com>
2026-06-24 09:12:56 +0500

    Add AI task: Talaba bahosini tekshirish dasturi

---
+++ ai_topshiriqlar/Talaba_bahosini_tekshirish_dasturi.py
@@ -0,0 +1,16 @@
+# Talaba bahosini tekshirish dasturi
+# Kurs: Dasturlash / IT
+# Mavzu: if, elif, else asoslari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+
+if n > 90:
+    print("A")
+elif n >= 70 and n < 89:
+    print("B")
+elif n >= 50 and n < 69:
+    print("C")
+else:
+    print("D")
