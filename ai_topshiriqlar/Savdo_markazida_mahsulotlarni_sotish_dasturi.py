9fc8f60f93c50fe899ee3c14563bb86bf73df238
xondamir-dev <xondamir917@gmail.com>
2026-06-24 13:54:27 +0500

    Add AI task: Savdo markazida mahsulotlarni sotish dasturi

---
+++ ai_topshiriqlar/Savdo_markazida_mahsulotlarni_sotish_dasturi.py
@@ -0,0 +1,15 @@
+# Savdo markazida mahsulotlarni sotish dasturi
+# Kurs: Dasturlash / IT
+# Mavzu: if, elif, else asoslari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+yosh = int(input())
+if yosh >= 18:
+    print("Sotishga ruhsat berildi")
+else:
+    r = input()
+    if r == "yes":
+        print("Sotishga ruhsat berildi")
+    else:
+        print("Sotishga ruhsat berilmadi")
