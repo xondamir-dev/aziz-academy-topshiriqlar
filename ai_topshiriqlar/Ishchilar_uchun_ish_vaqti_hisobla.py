25888f26a1580345b94446eef71933b3ecd5fb26
xondamir-dev <xondamir917@gmail.com>
2026-06-28 09:11:28 +0500

    Add AI task: Ishchilar uchun ish vaqti hisobla

---
+++ ai_topshiriqlar/Ishchilar_uchun_ish_vaqti_hisobla.py
@@ -0,0 +1,16 @@
+# Ishchilar uchun ish vaqti hisobla
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+i_b, i_t, s = map(int, input().split())
+u = i_t - i_b 
+if u > 8:
+    q = u - 8 
+else:
+    q = 0  
+    
+q_h = q * s 
+print(f"{u} soat, qo'shimcha haq {q_h}")
