163402d7faf09d2afec71c03f642c0527689e953
xondamir-dev <xondamir917@gmail.com>
2026-07-01 08:58:25 +0500

    Add AI task: Bank hisobvaraqlari bo'yicha foiz hisobi

---
+++ ai_topshiriqlar/Bank_hisobvaraqlari_boyicha_foiz_hisobi.py
@@ -0,0 +1,15 @@
+# Bank hisobvaraqlari bo'yicha foiz hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Sonlar: int va float — butun va kasr sonlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+m = float(input())
+
+if m < 10000:
+    f = m * 0.02 
+elif m < 50000 and m > 10001:
+    f = m * 0.03 
+else:
+    f = m * 0.04 
+print(f)
