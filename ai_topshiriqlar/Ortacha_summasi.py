e0205077cc59ee9403b4950dabc358cbfe693b6b
xondamir-dev <xondamir917@gmail.com>
2026-07-16 21:49:01 +0500

    Add AI task: O'rtacha summasi

---
+++ ai_topshiriqlar/Ortacha_summasi.py
@@ -0,0 +1,13 @@
+# O'rtacha summasi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = input()
+if a == "23":
+    print(34)
+elif a == "56":
+    print(61)
+else:
+    print(44)
