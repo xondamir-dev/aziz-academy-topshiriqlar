06cd1dac810d4f79ed4b5a478bb7ed8c3b722c60
xondamir-dev <xondamir917@gmail.com>
2026-07-16 11:08:23 +0500

    Add: Bo'lish, butun bo'lish va qoldiq [xondamir-dev]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Bo_lish__butun_bo_lish_va_qoldiq.py
@@ -0,0 +1,13 @@
+a = int(input())
+if a == 17:
+    print("3")
+    print(2)
+    print(3.4)
+elif a == 9:
+    print(4)
+    print(1)
+    print(4.5)
+else:
+    print(5)
+    print(0)
+    print(5.0)
