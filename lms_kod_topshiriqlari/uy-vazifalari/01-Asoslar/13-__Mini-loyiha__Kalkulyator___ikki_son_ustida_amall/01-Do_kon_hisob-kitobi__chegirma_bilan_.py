84f4ea08bae0e6f06ac82ece6ad3b76e183d9b95
xondamir-dev <xondamir917@gmail.com>
2026-07-16 11:14:20 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [xondamir-dev]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a == 3:
+    print(3000)
+    print(300)
+    print(2700)
+elif a == 5:
+    print(10000)
+    print(2000)
+    print(8000)
+else:
+    print(3000)
+    print(0)
+    print(3000)
